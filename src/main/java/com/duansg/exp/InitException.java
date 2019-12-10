a00652399e832699c89451cdf51a5508f89f0f9d
siguo.duan@melot.cn <Duansiguo001417>
2019-12-10 09:43:31 +0800

    //初始化异常

---
+++ src/main/java/com/duansg/exp/InitException.java
@@ -0,0 +1,12 @@
+package com.duansg.exp;
+
+public class InitException extends RuntimeException{
+
+    /**
+     * 构造器。
+     * @param message 消息
+     */
+    public InitException(String message) {
+        super(message);
+    }
+}
